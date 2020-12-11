9309d2f4bf4e2be7c1ac4a601fa51b1a77eb7a34
alavertu <adamlavertu@gmail.com>
2020-12-11 07:30:26 -0800

    trying out the new models

--- code/CovidFastFax.py
+++ code/CovidFastFax.py
@@ -164,8 +164,10 @@ class CovidFastFax(object):
         )
 
         ### SETUP FOR CHECKBOX COMPONENT
-        check_mu = (0.024431580375383045,)
-        check_std = (0.2114881181764639,)
+        # check_mu = (0.024431580375383045,)
+        # check_std = (0.2114881181764639,)
+        check_mu = (0.01628375922771153,)
+        check_std = (0.191671638865994,)
         self.chk_transf = transforms.Compose(
             [
                 transforms.ToPILImage(),
@@ -178,11 +180,11 @@ class CovidFastFax(object):
         # Need to update checkbox coordinates
 
         # Load models into ensemble predictor
-        model1 = load_model("../data/chkbox_models/chkbox_rnd7_0_bestVal.pt")
-        model2 = load_model("../data/chkbox_models/chkbox_rnd7_1_bestVal.pt")
-        model3 = load_model("../data/chkbox_models/chkbox_rnd7_2_bestVal.pt")
-        model4 = load_model("../data/chkbox_models/chkbox_rnd7_3_bestVal.pt")
-        model5 = load_model("../data/chkbox_models/chkbox_rnd7_4_bestVal.pt")
+        model1 = load_model("../data/chkbox_models/chkbox_rnd8_0_bestVal.pt")
+        model2 = load_model("../data/chkbox_models/chkbox_rnd8_1_bestVal.pt")
+        model3 = load_model("../data/chkbox_models/chkbox_rnd8_2_bestVal.pt")
+        model4 = load_model("../data/chkbox_models/chkbox_rnd8_3_bestVal.pt")
+        model5 = load_model("../data/chkbox_models/chkbox_rnd8_4_bestVal.pt")
 
         _ = model1.to(self.device)
         _ = model2.to(self.device)
